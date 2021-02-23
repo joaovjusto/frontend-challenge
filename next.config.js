f2e5525a094648bd434dc7b3c80923fb57db60df
Joao Justo <joao.justo@roit.com.br>
2021-02-23 12:37:23 -0300

    Fix path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: `${process.env.NEXT_PUBLIC_BASE_PATH}/static`,
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
