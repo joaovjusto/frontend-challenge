abbd86003269bf6a6c8f21797978d5552dbbf4d4
Joao Justo <joao.justo@roit.com.br>
2021-02-23 01:24:07 -0300

    Fix static generation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 const nextConfig = {
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+    : "",
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
